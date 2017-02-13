0717cdeb5df31cd80ae2530775297f492c9563e9
Hans Kurniadi <hans.d.k@live.com>
2017-02-13 20:05:40 +0000

    create a string buffer to save received data from stream and print to stdout the string

--- get-and-print-html.js
+++ get-and-print-html.js
@@ -4,17 +4,19 @@ function getAndPrintHTMLChunks () {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
-  var bufferArr = [];
+  //var bufferArr = [];
+  var bufferVar;
   https.get(requestOptions, function(response) {
     //set encoding of the received data to UTF-8
     response.setEncoding('utf8');
 
     //invoke callback when there is data to read
     response.on('data', function(data) {
-      bufferArr.push(data);
+      //bufferArr.push(data);
+      bufferVar += data;
       //console.log('This is the data received', data, "\n");
     });
 
@@ -25,8 +27,9 @@ function getAndPrintHTMLChunks () {
 
     //invoke callback when reaching the end of readable data
     response.on('end', function() {
-      console.log('Received data stored in a buffer array: ', bufferArr, '\n');
+      //console.log('Received data stored in a buffer array: ', bufferArr, '\n');
       //console.log('Received data stored in a buffer array at index 0: ', bufferArr[0]);
+      console.log('Received data stored in a buffer variable: ', bufferVar, '\n');
       console.log('Response stream is complete');
     });
   });
