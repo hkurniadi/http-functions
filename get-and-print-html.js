b686bd7a6485d71158cdbc9aa011e152bb8281ea
Hans Kurniadi <hans.d.k@live.com>
2017-02-13 19:48:57 +0000

    create a buffer array to store each chunk of data received

---
+++ get-and-print-html.js
@@ -0,0 +1,35 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  var bufferArr = [];
+  https.get(requestOptions, function(response) {
+    //set encoding of the received data to UTF-8
+    response.setEncoding('utf8');
+
+    //invoke callback when there is data to read
+    response.on('data', function(data) {
+      bufferArr.push(data);
+      //console.log('This is the data received', data, "\n");
+    });
+
+    //invoke error callback when there is error
+    response.on('error', function(err) {
+      console.log(err.stack);
+    });
+
+    //invoke callback when reaching the end of readable data
+    response.on('end', function() {
+      console.log('Received data stored in a buffer array: ', bufferArr, '\n');
+      //console.log('Received data stored in a buffer array at index 0: ', bufferArr[0]);
+      console.log('Response stream is complete');
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
