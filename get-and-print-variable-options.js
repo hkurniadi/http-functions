1004e536a8b87d049a8b58115eceb017366831ed
Hans Kurniadi <hans.d.k@live.com>
2017-02-13 20:18:35 +0000

    move option object outside the function, pass in option param to getprin function

---
+++ get-and-print-variable-options.js
@@ -0,0 +1,31 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks (option) {
+
+  https.get(option, function(response) {
+    //set encoding of the received data to UTF-8
+    response.setEncoding('utf8');
+
+    //invoke callback when there is data to read
+    response.on('data', function(data) {
+      console.log('This is the data received', data, "\n");
+    });
+
+    //invoke error callback when there is error
+    response.on('error', function(err) {
+      console.log(err.stack);
+    });
+
+    //invoke callback when reaching the end of readable data
+    response.on('end', function() {
+      console.log('Response stream is complete');
+    });
+  });
+}
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
+getAndPrintHTMLChunks(requestOptions);
